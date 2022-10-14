0abfe99f022bc16ab3005ae23d41edc450067dd4
ilya <fox367213@gmail.com>
2022-10-14 14:26:55 +0300

    Написание первой задачи

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+string a = Console.ReadLine()!;
+if (a.Length == 5)
+{
+    if (a[0] == a[4] && a[1] == a[3])
+    {
+        Console.WriteLine("Число является полиндромом");
+    }
+    else{
+        Console.WriteLine("Число не являеться полиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Введено неверное число");
+}
+
